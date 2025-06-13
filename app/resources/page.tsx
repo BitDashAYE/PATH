f4f57165deca592d0706b250a16d393d67d999e4
Destroy!! <sugam.arora14@gmail.com>
2025-06-14 01:53:03 +0530

    Fix empty page components

---
+++ app/resources/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function ResourcesPage() {
+  return (
+    <div>
+      <h1>Resources Page</h1>
+      <p>Welcome to the resources page!</p>
+    </div>
+  );
+}
